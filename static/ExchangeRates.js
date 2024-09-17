2af1429bafaf2dc50a99361eb9942e43d119f28e
Charles <charlesyounglau@gmail.com>
2024-09-17 12:17:54 -0700

    Update Exchange Rates script

--- static/ExchangeRates.js
+++ static/ExchangeRates.js
@@ -10,15 +10,12 @@ function getExchangeRates(){
 		} )
 		.catch((e) => console.error(e))
 
-	// var Json ='{"success": true,"timestamp": 1718739664,"base": "EUR","date": "2024-06-18","rates":{"USD": 1.073797,"EUR": 1,"CAD": 1.473088,"GBP": 0.844969,"JPY": 169.473,"CNY": 7.78997,"AUD": 1.61364,"HKD": 8.383077,"IDR": 17576.279831,"MXN": 19.773102,"SGD": 1.450876,"KRW": 1482.156173,"THB": 39.36572,"TWD": 34.745887}}'
+	// var Json ='{"success": true,"timestamp": 1718739664,"base": "EUR","date": "2024-06-18","rates":{"USD": 1.073797,"EUR": 1}}'
+
 	return JSON.parse(Json)
 }
 
 function printExchangeRates(){
-	// var Json = '{"success":true,"timestamp":1669218303,"base":"EUR","date":"2022-11-23","rates":{"EUR":1,"USD":1.037716,"GBP":0.860173,"JPY":145.172309,"CNY":7.438137,"AUD":1.545881,"HKD":8.112929,"IDR":16194.38492,"MXN":20.088001,"SGD":1.431098,"KRW":1393.693444,"THB":37.420139,"TWD":32.294543}}';
-	// var Json = '{"success":true,"timestamp":1706389983,"base":"EUR","date":"2024-01-27","rates":{"EUR":1,"USD":1.086361,"GBP":0.854797,"JPY":160.982486,"CNY":7.709476,"AUD":1.652512,"HKD":8.49301,"IDR":17137.620472,"MXN":18.641254,"SGD":1.459205,"KRW":1452.883297,"THB":38.707453,"TWD":33.990184}}'
-	// var Json = '{"success":true,"timestamp":1712258287,"base":"EUR","date":"2024-04-04","rates":{"EUR":1,"USD":1.084393,"GBP":0.857603,"JPY":163.974838,"CNY":7.845472,"AUD":1.645339,"HKD":8.489305,"IDR":17231.870307,"MXN":17.969688,"SGD":1.461279,"KRW":1463.073696,"THB":39.802354,"TWD":34.78819}}'
-	// var Json ='{"success": true,"timestamp": 1718739664,"base": "EUR","date": "2024-06-18","rates":{"USD": 1.073797,"EUR": 1,"CAD": 1.473088,"GBP": 0.844969,"JPY": 169.473,"CNY": 7.78997,"AUD": 1.61364,"HKD": 8.383077,"IDR": 17576.279831,"MXN": 19.773102,"SGD": 1.450876,"KRW": 1482.156173,"THB": 39.36572,"TWD": 34.745887}}'
 	
 	var obj = getExchangeRates();
 
