1e3025674edf33df889f86bceb41b9a440115313
Charles <charles.hin.lau@gmail.com>
2025-01-25 21:35:34 -0800

    Move all files to subdirectories Part 1

--- Security.py
+++
@@ -1,8 +0,0 @@
-''' File contain secret key '''
-# from ProjectA.Security import KEY
-
-# class Security:
-def KEY(): 
-    return 'AlohaFriday'
-    # return 'HappyHour'
-
--- runapp.py
+++ runapp.py
@@ -1,6 +1,14 @@
 """
-This script runs the ChlauApp application as an entry point.
+This script runs the ChlauApp application using a development server.
 """
 
-from . import app
-from . import views, admin
+from os import environ
+from ChlauWebApp import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
