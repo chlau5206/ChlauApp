190a4a857d7f804df8abe11a0e70aa55170c5758
Charles <charles.hin.lau@gmail.com>
2025-03-05 18:38:56 -0800

    minor update: requirements.txt and runapp.py

--- runapp.py
+++ runapp.py
@@ -4,21 +4,11 @@ This script is for the ChlauApp application's entry point.
 
 ''' runapp.py
 '''
-import sys
+import os, sys
 from ChlauApp import app
 
 if __name__ == '__main__':
-    #####################################
-    ## Visual Studio settings
-    # HOST = environ.get('SERVER_HOST', 'localhost')
-    # try:
-    #     PORT = int(environ.get('SERVER_PORT', '5000'))
-    # except ValueError:
-    #     PORT = 5555
     
-    # print (f'host={HOST};port={PORT}')
-    # app.run(HOST, PORT)
-
     #####################################
     ## Check Python at lease version 3.10
     print(f"Python {sys.version_info.major}.{sys.version_info.minor} is running")
@@ -26,9 +16,16 @@ if __name__ == '__main__':
         print("*** Python 3.10 or higher is required ***")
         sys.exit(1)    # fail. 
 
-        
-    
-    app.run()
-    
-    
+    # print(f"Template folder:  {app.jinja_env.loader.searchpath}")
+    # print(f"Static folder:    {app.static_folder}")
 
+    if app.config['FLASK_ENV'] == 'development':
+        DEBUG = app.config['DEBUG'] == True
+        HOST = os.environ.get('SERVER_HOST', 'localhost')
+        try:
+            PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        except ValueError:
+            PORT = 5555
+        app.run(HOST, PORT, debug=DEBUG)
+    else: 
+        app.run()
