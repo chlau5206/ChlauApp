469377af60e0b599e2c7578d6570966ede9670d6
Charles <charles.hin.lau@gmail.com>
2026-02-13 20:52:50 -0800

    Update Contact me and create MsgBox - contactMe for Project

--- ChlauApp/Board/__init__.py
+++
@@ -1,8 +0,0 @@
-# Board/__init__.py  
-
-from flask import Blueprint
-
-board_bp = Blueprint('board_bp', __name__, template_folder= 'templates', static_folder='static')
-
-from . import Board  # Import routes to register them with the blueprint
-
