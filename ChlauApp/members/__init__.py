7fe48088c966e50ac0cc4dc24f36d0e4e7dfe0e7
Charles <charles.hin.lau@gmail.com>
2026-02-13 20:45:27 -0800

    remove Admin codes from main

--- ChlauApp/members/__init__.py
+++
@@ -1,7 +0,0 @@
-# members/__init__.py  
-
-from flask import Blueprint
-
-members_bp = Blueprint('members_bp', __name__, template_folder= 'templates', static_folder='static')
-
-from . import members  # Import routes to register them with the blueprint
