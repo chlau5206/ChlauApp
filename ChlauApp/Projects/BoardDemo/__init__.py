736c5ccff61b1fb62d036e6f3af98fa432614780
Charles <charles.hin.lau@gmail.com>
2026-02-18 09:26:35 -0800

    Refactoring Final Code completed

--- ChlauApp/Projects/BoardDemo/__init__.py
+++
@@ -1,8 +0,0 @@
-# BoardDemo/__init__.py  
-
-from flask import Blueprint
-
-boardDemo_bp = Blueprint('boardDemo_bp', __name__, template_folder= 'templates', static_folder='static')
-
-from . import demoBoard  # Import routes to register them with the blueprint
-
