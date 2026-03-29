f23caf88883e20727e9ae63ba091f594b44c8d9c
Charles <charles.hin.lau@gmail.com>
2026-03-28 18:19:42 -0700

    Fix bug: resume download

--- ChlauApp/About2/about2.py
+++ ChlauApp/About2/about2.py
@@ -1,7 +1,6 @@
 # about/about2.py -- Contains the routes and CRUD operations
-
-
-from flask import render_template
+import os
+from flask import render_template , send_from_directory
 # from flask import redirect, url_for, flash, current_app, request
 # from flask_login import login_required 
 
@@ -16,3 +15,8 @@ def index():
     return render_template("About21.html", 
                            title="About me (v2)")
 
+@about2_bp.route("/resume")
+def download_resume():
+    about2_bp.root_path
+    data_folder = os.path.join(about2_bp.root_path, "data")
+    return send_from_directory(data_folder, "current_resume.pdf", as_attachment=True)
